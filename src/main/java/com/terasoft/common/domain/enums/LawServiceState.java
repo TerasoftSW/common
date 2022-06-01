875a68206e32e2b503f09e8b8a2536cfb55fdcb1
Mauricio Carmen <81581259+mauricio-gt23@users.noreply.github.com>
2022-05-31 23:04:31 -0500

    add: Implement Common

---
+++ src/main/java/com/terasoft/common/domain/enums/LawServiceState.java
@@ -0,0 +1,14 @@
+package com.terasoft.common.domain.enums;
+
+public enum LawServiceState {
+    INPROCESS (1),
+    FINISHED(2);
+
+    private final int value;
+
+    LawServiceState(final int state) { value = state; }
+
+    public int getValue() { return value; }
+
+}
+
